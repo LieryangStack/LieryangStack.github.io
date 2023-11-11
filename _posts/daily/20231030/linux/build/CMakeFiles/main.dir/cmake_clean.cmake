76fecf77981706da9a767677753bc3602ed2d0de
LieryangStack <SXlisibo@gmail.com>
2023-11-11 22:16:06 +0800

    update

---
+++ _posts/daily/20231030/linux/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/WzSerialPort.cpp.o"
+  "CMakeFiles/main.dir/interface.cpp.o"
+  "CMakeFiles/main.dir/main.c.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
