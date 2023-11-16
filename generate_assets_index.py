885049f97c543388595fa39eccc4076466226198
LieryangStack <sxlisibo@gmail.com>
2023-11-16 18:12:44 +0800

    update

--- generate_assets_index.py
+++ generate_assets_index.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import os
 
-def create_index_html(dir_path, subdirs_files):
+def create_index_html(dir_path, subdirs, files):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
@@ -14,10 +15,16 @@ title: %s Index
 <ul>
 """ % (title, title)
 
-    for subdir in subdirs_files:
-        subdir_path = os.path.join(dir_path, subdir)
-        link_path = subdir_path.replace('assets/', '')  # Adjust the path for the HTML link
-        content += '  <li><a href="{{ site.baseurl }}/assets/%s/">%s</a></li>\n' % (link_path, subdir)
+    # Create links for directories
+    for subdir in subdirs:
+        link_path = os.path.join(dir_path, subdir).replace('assets/', '')  # Adjust the path for the HTML link
+        content += '  <li><a href="{{ site.baseurl }}/assets/%s/">%s/</a></li>\n' % (link_path, subdir)
+
+    # Create download links for files
+    for file in files:
+        link_path = os.path.join(dir_path, file).replace('assets/', '')  # Adjust the path for the HTML link
+        content += '  <li><a href="{{ site.baseurl }}/assets/%s" download>%s</a></li>\n' % (link_path, file)
+
     content += "</ul>"
 
     with open(os.path.join(dir_path, "index.html"), "w") as f:
@@ -25,9 +32,12 @@ title: %s Index
 
 def process_directory(path, base_path=''):
     for root, dirs, files in os.walk(path, topdown=True):
-        subdirs_files = dirs + files
+        # Ignore index.html in the root directory
+        if root == path and "index.html" in files:
+            files.remove("index.html")
+
         dir_path = os.path.join(base_path, os.path.relpath(root, path)) if base_path else root
-        create_index_html(dir_path, subdirs_files)
+        create_index_html(dir_path, dirs, files)
 
 def main():
     assets_dir = "assets"
