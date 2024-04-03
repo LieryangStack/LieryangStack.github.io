a89b16afba771608e8ba2ddeb5332e5b6020dcab
LieryangStack <sxlisibo@gmail.com>
2024-04-03 09:08:29 +0800

    update

---
+++ assets/CPlusPlus/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lieryang/Desktop/LieryangStack.github.io/assets/CPlusPlus/01_namespace/test04/h1.cpp" "CMakeFiles/main.dir/h1.cpp.obj" "gcc" "CMakeFiles/main.dir/h1.cpp.obj.d"
+  "C:/Users/lieryang/Desktop/LieryangStack.github.io/assets/CPlusPlus/01_namespace/test04/main.cpp" "CMakeFiles/main.dir/main.cpp.obj" "gcc" "CMakeFiles/main.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
