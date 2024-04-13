9c1afab39f00e872ac53406589d119118977bb14
LieryangStack <sxlisibo@gmail.com>
2024-04-13 17:33:35 +0800

    update

---
+++ assets/BuildSystem/Cmake/StageTwo/2024041302/Sample01/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lieryang/Documents/Sample01/src/3.c" "/home/lieryang/Documents/Sample01/build/CMakeFiles/main.dir/src/3.c.o"
+  "/home/lieryang/Documents/Sample01/src/4.c" "/home/lieryang/Documents/Sample01/build/CMakeFiles/main.dir/src/4.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lieryang/Documents/Sample01/src/1.cpp" "/home/lieryang/Documents/Sample01/build/CMakeFiles/main.dir/src/1.cpp.o"
+  "/home/lieryang/Documents/Sample01/src/2.cpp" "/home/lieryang/Documents/Sample01/build/CMakeFiles/main.dir/src/2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ assets/BuildSystem/Cmake/StageTwo/2024041302/Sample01/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/1.cpp.o"
+  "CMakeFiles/main.dir/src/2.cpp.o"
+  "CMakeFiles/main.dir/src/3.c.o"
+  "CMakeFiles/main.dir/src/4.c.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
