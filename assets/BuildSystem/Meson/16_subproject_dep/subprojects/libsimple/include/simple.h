f6ce013be5fc722619f31fba94c4d0814959fd0c
LieryangStack <sxlisibo@gmail.com>
2024-06-29 14:04:11 +0800

    update

--- assets/BuildSystem/Meson/16_subproject_dep/subprojects/libsimple/include/simple.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SIMPLE_H__
-#define __SIMPLE_H__
-
-void simple_print();
-
-#endif
--- assets/BuildSystem/Meson/16_subproject_dep/subprojects/libsimple/simple.cpp
+++
@@ -1,7 +0,0 @@
-#include "simple.h"
-#include <stdio.h>
-
-void 
-simple_print() {
-	printf("Hello there.\n");
-}
